df9816f35da2df7284ba70fd893216cd44bde927
Marcelo  Montenegro <montenegromf@gmail.com>
2020-02-12 00:13:10 +0100

    this is a new website
    this is a new website with some style css and some JS

---
+++ script.js
@@ -0,0 +1,2 @@
+<a href="https://www.freecounterstat.com" title="hit counters for websites"><img
+src="https://counter10.wheredoyoucomefrom.ovh/private/freecounterstat.php?c=jjh9t8jjldzl97ny7p1f2c91hcw3stc4" border="0" title="hit counters for websites" alt="hit counters for websites"></a>
